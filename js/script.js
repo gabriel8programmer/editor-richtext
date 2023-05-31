2b99339087a437aa3ca6d0f0ba1b8e63a4fdcb63
gabriel8programmer <gabriel8github@gmail.com>
2023-05-30 23:07:38 -0300

    created html and css for the project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+const buttons = {
+    buttonBold: document.getElementById("bold"),
+    buttonItalic: document.getElementById("italic"),
+    buttonUnderline: document.getElementById("underline"),
+    buttonAlignLeft: document.getElementById("left"),
+    buttonAlignCenter: document.getElementById("center"),
+    buttonAlignRight: document.getElementById("right"),
+    buttonAlignJustify: document.getElementById("justify"),
+    buttonOL: document.getElementById("ol"),
+    buttonUL: document.getElementById("ul"),
+}
