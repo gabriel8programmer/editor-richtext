87606dfdcb8c9e93eca1e1a32253c4cf34589802
gabriel8programmer <gabriel8github@gmail.com>
2023-06-06 23:37:24 -0300

    finish new css for editor

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 
-const buttons = {
-    buttonBold: document.getElementById("bold"),
-    buttonItalic: document.getElementById("italic"),
-    buttonUnderline: document.getElementById("underline"),
-    buttonAlignLeft: document.getElementById("left"),
-    buttonAlignCenter: document.getElementById("center"),
-    buttonAlignRight: document.getElementById("right"),
-    buttonAlignJustify: document.getElementById("justify"),
-    buttonOL: document.getElementById("ol"),
-    buttonUL: document.getElementById("ul"),
+const buttons = document.querySelectorAll("button")
+const tagsI = document.querySelectorAll("i")
+const bodyEditor = document.querySelector("#body")
+
+const editor = {
+    currentText: "",
+    textSelected: ""
 }
+
+buttons.forEach(button=>{
+    button.addEventListener("click", (e)=>{
+        e.stopPropagation()
+        bodyEditor.focus()
+    })
+})
+
+tagsI.forEach(button=>{
+    button.addEventListener("click", (e)=>{
+        e.target.parentNode.classList.toggle("actived")
+    })
+})
+
+bodyEditor.addEventListener("select", (e)=>{
+    editor.currentText = e.target.value
+    editor.textSelected = editor.currentText.slice(e.target.selectionStart, e.target.selectionEnd)
+    console.log(editor.currentText, editor.textSelected)
+})
